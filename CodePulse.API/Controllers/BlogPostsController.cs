72f62f7d1756cf81ce7d5ccc682e62ff77e0a470
Success Imamun <SuccessImamun@gmail.com>
2023-11-25 18:05:45 +0100

    Added CreateBlogPost Endpoint

---
+++ CodePulse.API/Controllers/BlogPostsController.cs
@@ -0,0 +1,55 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostsController : ControllerBase
+    {
+        private readonly IBlogPostRepository blogPostRepository;
+
+        public BlogPostsController(IBlogPostRepository blogPostRepository)
+        {
+            this.blogPostRepository = blogPostRepository;
+        }
+
+        // POST: {apibaseurl}/api/blogposts
+        [HttpPost]
+        public async Task<IActionResult> CreateBlogPost([FromBody] CreateBlogPostRequestDto request)
+        {
+            // From Dto to Domain
+            var blogPost = new Blogpost
+            {
+                Title = request.Title,
+                ShortDescription = request.ShortDescription,
+                Content = request.Content,
+                FeaturedImageUrl = request.FeaturedImageUrl,
+                UrlHandle = request.UrlHandle,
+                PublishedDate = request.PublishedDate,
+                Author = request.Author,
+                IsVisible = request.IsVisible,
+            };
+
+            blogPost = await blogPostRepository.CreateAsync(blogPost);
+            
+            // Convert Domain Object back to DTO
+            var response = new BlogPostDto
+            {
+                Id = blogPost.Id,
+                Title = blogPost.Title,
+                ShortDescription = blogPost.ShortDescription,
+                Content = blogPost.Content,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                UrlHandle = blogPost.UrlHandle,
+                PublishedDate = blogPost.PublishedDate,
+                Author = blogPost.Author,
+                IsVisible = blogPost.IsVisible,
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ CodePulse.API/Repositories/Implementation/BlogPostRepository.cs
@@ -0,0 +1,23 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+
+namespace CodePulse.API.Repositories.Implementation
+{
+    public class BlogPostRepository : IBlogPostRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public BlogPostRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<Blogpost> CreateAsync(Blogpost blogpost)
+        {
+            await dbContext.Blogposts.AddAsync(blogpost);
+            await dbContext.SaveChangesAsync();
+            return blogpost;
+        }
+    }
+}
